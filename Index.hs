1761c2f0d5ecb1ffbaeeece61bee8d758d9893e2
Max Goldstein <maxgoldstein1@gmail.com>
2014-06-05 14:06:15 -0400

    Aesthetic reformat of the index page.
    Split files by type, catalog-inspired directory nav, hide dotfile, build, cache.
    A basis for adding debugger and editor support.

--- Index.hs
+++ Index.hs
@@ -5,6 +5,7 @@ import Control.Monad.IO.Class
 import qualified Data.ByteString as BS
 import qualified Data.ByteString.Char8 as S
 import Data.List
+import Data.List.Split (splitOn)
 import System.Directory
 import System.FilePath
 import System.PosixCompat.Files
@@ -16,14 +17,17 @@ indexStyle :: BS.ByteString
 indexStyle = S.pack
     "body { margin:0; font-family:sans-serif; background:rgb(245,245,245);\
     \       font-family: calibri, verdana, helvetica, arial; }\
-    \div.header { padding: 40px 50px; font-size: 24px; }\
+    \div.topbar {height: 6px; background-color: rgb(90,99,120); }\
+    \div.intro  { padding: 20px 50px 0px; font-size: 14px; color:#888 }\
+    \div.header { padding: 0px 50px 20px; font-size: 24px; color:rgb(78,78,78) }\
     \div.content { padding: 0 40px }\
-    \table { width:100%; border-collapse:collapse; }\
-    \td { padding: 6px 10px; }\
-    \tr:nth-child(odd) { background:rgb(216,221,225); }\
-    \td { font-family:monospace }\
+    \table { width:100%; border-collapse:collapse; margin-bottom: 40px; float: left }\
+    \table.half { width:50% }\
+    \a { text-decoration: none; color:rgb(90,99,120) }\
+    \td { padding: 6px 0px; color:rgb(78,78,78) }\
+    \tr { border-bottom: solid rgb(216, 221, 225) 1px }\
     \th { background:rgb(90,99,120); color:white; text-align:left;\
-    \     padding:10px; font-weight:normal; }"
+    \     padding:3px 10px; font-weight:normal; }"
 
 elmIndexGenerator :: MonadSnap m
                       => FilePath   -- ^ Directory to generate index for
@@ -32,30 +36,63 @@ elmIndexGenerator d = let writeS = writeBS . S.pack in do
     modifyResponse $ setContentType $ S.pack "text/html"
 
     rq      <- getRequest
+    let uri = S.unpack $ rqURI rq
 
     writeS "<style type='text/css'>"
     writeBS indexStyle
-    writeS "</style><div class=\"header\">Directory Listing: "
-    writeBS (rqURI rq)
+    writeS "</style><div class=\"topbar\"></div>"
+    writeS "<div class=\"intro\">Directory Listing</div>"
+    writeS "<div class=\"header\">"
+    writeS "<a href='/'>~</a>/"
+    when (uri /= "/") $ do
+        let path = splitOn "/" uri
+        let pathScan = scanr1 (\a b -> a ++ ('/':b)) path
+        mapM_ (\(p,ps) -> unless (null p) $ writeS $ "<a href='"++ps++"'>"++p++"</a>/") (zip path pathScan)
     writeS "</div><div class=\"content\">"
-    writeS "<table><tr><th>File Name</th><th>Type</th><th>Last Modified"
-    writeS "</th></tr>"
-
-    when (rqURI rq /= S.pack "/") $
-        writeS "<tr><td><a href='../'>..</a></td><td colspan=2>DIR</td></tr>"
 
     entries <- liftIO $ getDirectoryContents d
     dirs    <- liftIO $ filterM (doesDirectoryExist . (d </>)) entries
     files   <- liftIO $ filterM (doesFileExist . (d </>)) entries
 
-    forM_ (sort $ filter (not . (`elem` ["..", "."])) dirs) $ \f -> do
+    let (elmFiles, notElmFiles) = partition (isSuffixOf ".elm") files
+    let isHtml5 f = any (flip isSuffixOf f) [".html", ".css", "js"]
+    let (html5Files, otherFiles) = partition isHtml5 notElmFiles
+
+    unless (null elmFiles) $ writeS "<table><tr><th>Elm File</th><th>Last Modified</th></tr>"
+    forM_ (sort elmFiles ) $ \f -> do
+        stat <- liftIO $ getFileStatus (d </> f)
+        tm   <- liftIO $ formatHttpTime (modificationTime stat)
         writeS "<tr><td><a href='"
         writeS f
-        writeS "/'>"
+        writeS "'>"
         writeS f
-        writeS "</a></td><td colspan=2>DIR</td></tr>"
+        writeS "</a></td><td>"
+        writeBS tm
+        writeS "</td></tr>"
+    unless (null elmFiles) $ writeS "</table>"
 
-    forM_ (sort files) $ \f -> do
+    let dotFile f = null f || head f == '.'
+    let keepDir d = d /= "cache" && d /= "build" && not (dotFile d)
+    writeS "<table class=\"half\"><tr><th>Directory Name</th></tr>"
+    forM_ (sort $ filter keepDir dirs) $ \d -> do
+        writeS "<tr><td><a href='"
+        writeS d
+        writeS "/'>"
+        writeS d
+        writeS "</a></tr>"
+    writeS "</table>"
+
+    writeS "<table class=\"half\"><tr><th>HTML5 File</th></tr>"
+    forM_ (sort html5Files) $ \d -> do
+        writeS "<tr><td><a href='"
+        writeS d
+        writeS "/'>"
+        writeS d
+        writeS "</a></tr>"
+    writeS "</table>"
+
+    writeS "<table><tr><th>File Name</th><th>MIME type</th><th>Last Modified</th></tr>"
+    forM_ (sort $ filter (not . dotFile) otherFiles) $ \f -> do
         stat <- liftIO $ getFileStatus (d </> f)
         tm   <- liftIO $ formatHttpTime (modificationTime stat)
         writeS "<tr><td><a href='"
