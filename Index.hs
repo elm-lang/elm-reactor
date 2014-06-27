b0d9c3dfb486d395fcaa4a89b6efd716270a11ce
Max Goldstein <maxgoldstein1@gmail.com>
2014-06-26 22:39:53 -0400

    Don't separate out HTML5 files, per @evancz

--- Index.hs
+++ Index.hs
@@ -23,9 +23,8 @@ indexStyle = S.pack
     \div.header { padding: 0px 50px 20px; font-size: 24px; color:rgb(78,78,78) }\
     \div.content { padding: 0 40px }\
     \table { width:100%; border-collapse:collapse; margin-bottom: 40px; float: left }\
-    \table.half { width:50% }\
     \a { text-decoration: none; color:rgb(90,99,120) }\
-    \td { padding: 6px 0px; color:rgb(78,78,78) }\
+    \td { padding: 6px 10px; color:rgb(78,78,78) }\
     \tr { border-bottom: solid rgb(216, 221, 225) 1px }\
     \th { background:rgb(90,99,120); color:white; text-align:left;\
     \     padding:3px 10px; font-weight:normal; }"
@@ -57,54 +56,48 @@ elmIndexGenerator d = do
     dirs    <- liftIO $ filterM (doesDirectoryExist . (d </>)) entries
     files   <- liftIO $ filterM (doesFileExist . (d </>)) entries
 
-    let (elmFiles, notElmFiles) = partition (isSuffixOf ".elm") files
+    let (elmFiles, otherFiles) = partition (isSuffixOf ".elm") files
     let isHtml5 f = any (flip isSuffixOf f) [".html", ".css", "js"]
-    let (html5Files, otherFiles) = partition isHtml5 notElmFiles
 
-    unless (null elmFiles) $ writeS "<table><tr><th>Elm File</th><th>Last Modified</th></tr>"
-    forM_ (sort elmFiles ) $ \f -> do
-        tm <- liftIO . getModificationTime $ d </> f
-        writeS "<tr><td><a href='"
-        writeS f
-        writeS "'>"
-        writeS f
-        writeS "</a></td><td>"
-        writeS $ formatTime' tm
-        writeS "</td></tr>"
-    unless (null elmFiles) $ writeS "</table>"
+    unless (null elmFiles) $ do
+        writeS "<table><tr><th>Elm File</th><th>Last Modified</th></tr>"
+        forM_ (sort elmFiles ) $ \f -> do
+            tm <- liftIO . getModificationTime $ d </> f
+            writeS "<tr><td><a href='"
+            writeS f
+            writeS "'>"
+            writeS f
+            writeS "</a></td><td>"
+            writeS $ formatTime' tm
+            writeS "</td></tr>"
+        writeS "</table>"
 
     let dotFile f = null f || head f == '.'
     let keepDir d = d /= "cache" && d /= "build" && not (dotFile d)
-    writeS "<table class=\"half\"><tr><th>Directory Name</th></tr>"
-    forM_ (sort $ filter keepDir dirs) $ \d -> do
-        writeS "<tr><td><a href='"
-        writeS d
-        writeS "/'>"
-        writeS d
-        writeS "</a></tr>"
-    writeS "</table>"
+    let dirs' = sort $ filter keepDir dirs
+    unless (null dirs') $ do
+        writeS "<table><tr><th>Directory Name</th></tr>"
+        forM_ dirs' $ \d -> do
+            writeS "<tr><td><a href='"
+            writeS d
+            writeS "/'>"
+            writeS d
+            writeS "</a></tr>"
+        writeS "</table>"
 
-    writeS "<table class=\"half\"><tr><th>HTML5 File</th></tr>"
-    forM_ (sort html5Files) $ \d -> do
-        writeS "<tr><td><a href='"
-        writeS d
-        writeS "/'>"
-        writeS d
-        writeS "</a></tr>"
-    writeS "</table>"
-
-    writeS "<table><tr><th>File Name</th><th>MIME type</th><th>Last Modified</th></tr>"
-    forM_ (sort $ filter (not . dotFile) otherFiles) $ \f -> do
-        tm <- liftIO . getModificationTime $ d </> f
-        writeS "<tr><td><a href='"
-        writeS f
-        writeS "'>"
-        writeS f
-        writeS "</a></td><td>"
-        writeBS $ fileType defaultMimeTypes f
-        writeS "</td><td>"
-        writeS $ formatTime' tm
-        writeS "</tr>"
-
-    writeS "</table></div>"
+    let otherFiles' = sort $ filter (not . dotFile) otherFiles
+    unless (null otherFiles') $ do
+        writeS "<table><tr><th>File Name</th><th>MIME type</th><th>Last Modified</th></tr>"
+        forM_ otherFiles' $ \f -> do
+            tm <- liftIO . getModificationTime $ d </> f
+            writeS "<tr><td><a href='"
+            writeS f
+            writeS "'>"
+            writeS f
+            writeS "</a></td><td>"
+            writeBS $ fileType defaultMimeTypes f
+            writeS "</td><td>"
+            writeS $ formatTime' tm
+            writeS "</tr>"
+        writeS "</table></div>"
 
