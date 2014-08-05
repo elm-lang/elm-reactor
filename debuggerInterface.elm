40bd9637c71c91e2614f1b8faffe79ef9d8052ec
Michael James <michael@michaelbjames.com>
2014-08-05 16:52:26 +0200

    we use a native slider

--- debuggerInterface.elm
+++ debuggerInterface.elm
@@ -2,22 +2,17 @@ module DebuggerInterface where
 
 import Window
 import Graphics.Input (..)
-import Mouse
 import Debug
+import Slider (..)
 
 -- Model
 
-type Mouse =
-    { down : Bool
-    , position : (Float, Float)
-    }
-
 type Update =
-    { mouse : Mouse
-    , window : (Int, Int)
-    , maxEvents : Int
+    { window : (Int, Int)
     , paused : Bool
     , restart : Bool
+    , maxEvents : Int
+    , scrub : Int
     }
 
 type State =
@@ -25,24 +20,18 @@ type State =
     , events : (Int, Int)
     }
 
-{-
-data Action
-    = Noop
-    | Pause
-    | Continue
-    | Restart
-    | Scrub Int
--}
-
 pausedInput : Input Bool
 pausedInput = input False
 
 restartInput : Input ()
 restartInput = input ()
 
+scrubInput : Input Int
+scrubInput = input 0
+
 -- inport placeholder
-port eventCounter : Signal Int
---eventCounter = count <| fps 2
+--port eventCounter : Signal Int
+eventCounter = count <| fps 1
 
 -- View
 
@@ -73,64 +62,33 @@ restartButton =
     in  collage buttonWidth objHeight icon
             |> clickable restartInput.handle ()
 
-timelinePath : (Int, Int) -> Element
-timelinePath (w,_) =
-    let w' = toFloat w
-        lengthOffset = 100
-        emptyTimeline = rect (w' - lengthOffset) 2
-            |> filled black
-            |> moveX 15
-    in  collage (w - lengthOffset) objHeight [emptyTimeline]
-
-linearScale : (Float, Float) -> (Float, Float) -> Float -> Float
-linearScale (domainMin, domainMax) (rangeMin, rangeMax) x =
-    rangeMin + ((rangeMax - rangeMin) * (x - domainMin) / (domainMax - domainMin))
-
-timelineFilled : (Int, Int) -> State -> Element
-timelineFilled (w,_) state =
-    let w' = toFloat w
-        lengthOffset = 100
-        collageOffset = 10
-        len = w' - lengthOffset
-        currentLocation = case state.paused of
-            True ->
-                let max = toFloat <| snd state.events
-                    curr = toFloat <| fst state.events
-                in  linearScale (0,max) (-len/2 + 17,len/2) curr
-            False -> w' / 2 - 65 + collageOffset
-        indicator = rect 10 25
-            |> filled blue
-            |> moveX currentLocation
-    in  collage (w - lengthOffset) objHeight [indicator]
+scrubSlider : (Int, Int) -> State -> Element
+scrubSlider (w,_) state =
+    let leftOffset = sum <| map widthOf [playButton, restartButton]
+        sliderLength = w - leftOffset
+        sliderStyle =
+            { defaultSlider
+            | length <- sliderLength
+            , max <- toFloat <| snd state.events
+            , value <- toFloat <| fst state.events
+            , disabled <- not state.paused
+            }
+        sliderStyle' =
+            if  | state.paused ->
+                    sliderStyle
+                | otherwise ->
+                    { sliderStyle
+                    | value <- toFloat <| fst state.events
+                    }
+        _ = Debug.watch "value" sliderStyle'.value
+    in  container sliderLength objHeight midLeft
+            <| slider scrubInput.handle round sliderStyle'
 
 view : (Int, Int) -> State -> Element
 view dim state =
-    (case state.paused of
-        True -> playButton
-        False -> pauseButton)
+    (if state.paused then playButton else pauseButton)
     `beside` restartButton
-    `beside`  layers [ timelinePath dim
-                     , timelineFilled dim state
-                     ]
-
--- Calcuations from position
-
-mouseOnTimeline : Update -> Maybe Int
-mouseOnTimeline update =
-    let mx = fst update.mouse.position
-        leftOffset = sum <| map widthOf [playButton, restartButton]
-        mx' = mx - toFloat leftOffset
-        timeLineWidth = toFloat <| widthOf <| timelinePath update.window
-        scale m = linearScale (15, timeLineWidth) (0, toFloat update.maxEvents) m
-    in  case mx' >= 15 && mx' <= timeLineWidth of
-            True -> Just <| round <| scale mx'
-            False -> Nothing
-
-sliderPosition : Update -> Int
-sliderPosition update =
-    case mouseOnTimeline update of
-        Just n -> n
-        Nothing -> update.maxEvents
+    `beside` scrubSlider dim state
 
 -- The wiring
 
@@ -138,7 +96,7 @@ main : Signal Element
 main = lift2 view Window.dimensions scene
 
 port scrub : Signal Int
-port scrub = dropRepeats <| lift (fst . .events) scene
+port scrub = scrubInput.signal
 
 port pause : Signal Bool
 port pause = pausedInput.signal
@@ -151,13 +109,17 @@ scene = foldp step startState aggregateUpdates
 
 step : Update -> State -> State
 step update state =
-    let currentEvent = sliderPosition update
-    in  case update.restart of
-            True -> startState
-            False ->
-                { paused = update.paused
-                , events = (currentEvent, update.maxEvents)
-                }
+    if  | Debug.watch "case step" update.restart ->
+            startState
+        | otherwise ->
+            if  | update.paused ->
+                    { paused = update.paused
+                    , events = (update.scrub, update.maxEvents)
+                    }
+                | otherwise ->
+                    { paused = update.paused
+                    , events = (update.maxEvents, update.maxEvents)
+                    }
 
 startState : State
 startState =
@@ -167,19 +129,17 @@ startState =
 
 aggregateUpdates : Signal Update
 aggregateUpdates =
-    let dampenedMouse = keepWhen Mouse.isDown (0,0) Mouse.position
-        dampenedEvents = keepWhen (lift (not) pausedInput.signal) 0 eventCounter
-        restartBool = merge (sampleOn combinedMouse <| constant False) (lift (\x -> True) restartInput.signal)
-        mouse down (x,y) =
-            { down = down
-            , position = (toFloat x, toFloat y)
-            }
-        combinedMouse = lift2 mouse Mouse.isDown dampenedMouse
-        aggregator window mouse paused restart eventCounter =
-            { mouse = mouse
-            , window = window
+    let dampenedEvents = keepWhen (lift not pausedInput.signal) 0 eventCounter
+        restartBool = merge (sampleOn (fps 60) (constant False)) (lift (\x -> True) restartInput.signal)
+        aggregator window paused restart eventCounter scrubPosition =
+            { window = window
+            , paused = Debug.watch "paused" paused
+            , restart = Debug.watch "restart" restart
             , maxEvents = eventCounter
-            , paused = paused
-            , restart = restart
+            , scrub = Debug.watch "scrub position" scrubPosition
             }
-    in  lift5 aggregator Window.dimensions combinedMouse pausedInput.signal restartBool dampenedEvents
+    in  aggregator <~ Window.dimensions
+                    ~ pausedInput.signal
+                    ~ restartBool
+                    ~ dampenedEvents
+                    ~ scrubInput.signal
