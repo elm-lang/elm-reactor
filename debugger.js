eb0814e36671250a396e9d3bcc919af9fdb5b861
Michael James <michael@michaelbjames.com>
2014-08-08 15:53:48 +0200

    use modified toString

--- debugger.js
+++ debugger.js
@@ -161,14 +161,15 @@ function sendWatches(position) {
 
     for(key in watchAtPoint) {
         var value = watchAtPoint[key];
-        var jsonWatch = JSON.stringify(value, censor, separator);
+        // var jsonWatch = JSON.stringify(value, censor, separator);
+        var stringified = toString(value, separator);
         // Thanks http://stackoverflow.com/questions/11233498/json-stringify-without-quotes-on-properties
-        jsonWatch.replace(/\\"/g,"\uFFFF"); // U+FFFF
-        jsonWatch = jsonWatch.replace(/\"([^"]+)\":/g,"$1 =").replace(/\uFFFF/g,"\\\"");
-        if (typeof value === "object" || typeof value === "array") {
-            jsonWatch = jsonWatch.substr(2, jsonWatch.length - 4); // Removes outer {}
-        }
-        output.push([key, jsonWatch]);
+        // jsonWatch.replace(/\\"/g,"\uFFFF"); // U+FFFF
+        // jsonWatch = jsonWatch.replace(/\"([^"]+)\":/g,"$1 =").replace(/\uFFFF/g,"\\\"");
+        // if (typeof value === "object" || typeof value === "array") {
+            // jsonWatch = jsonWatch.substr(2, jsonWatch.length - 4); // Removes outer {} or []
+        // }
+        output.push([key, stringified]);
     }
     
     debuggerHandle.ports.watches.send(output);
@@ -230,4 +231,110 @@ function hotSwap(raw) {
         var mainNode = document.getElementById(ELM_MAIN_ID);
         mainNode.parentElement.appendChild(errorNode);
     }
+}
+
+
+var toString = function(v, separator) {
+    var type = typeof v;
+    if (type === "function") {
+        var name = v.func ? v.func.name : v.name;
+        return '<function' + (name === '' ? '' : ': ') + name + '>';
+    } else if (type === "boolean") {
+        return v ? "True" : "False";
+    } else if (type === "number") {
+        return v.toFixed(2).replace(/\.0+$/g, '');
+    } else if ((v instanceof String) && v.isChar) {
+        return "'" + addSlashes(v) + "'";
+    } else if (type === "string") {
+        return '"' + addSlashes(v) + '"';
+    } else if (type === "object" && '_' in v && probablyPublic(v)) {
+        var output = [];
+        for (var k in v._) {
+            for (var i = v._[k].length; i--; ) {
+                output.push(k + " = " + toString(v._[k][i], separator));
+            }
+        }
+        for (var k in v) {
+            if (k === '_') continue;
+            output.push(k + " = " + toString(v[k], separator));
+        }
+        if (output.length === 0) return "{}";
+        var body = "\n" + output.join(",\n");
+        return "{" + body.replace(/\n/g,"\n" + separator) + "\n}";
+    } else if (type === "object" && 'ctor' in v) {
+        if (v.ctor.substring(0,6) === "_Tuple") {
+            var output = [];
+            for (var k in v) {
+                if (k === 'ctor') continue;
+                output.push(toString(v[k], separator));
+            }
+            return "(" + output.join(", ") + ")";
+        } else if (v.ctor === "_Array") {
+            var list = Array.toList(v);
+            return "Array.fromList " + toString(list, separator);
+        } else if (v.ctor === "::") {
+            var output = '[\n' + toString(v._0, separator);
+            v = v._1;
+            while (v.ctor === "::") {
+                output += ",\n" + toString(v._0, separator);
+                v = v._1;
+            }
+            output += ' ]';
+            return output.replace(/\n/g,"\n" + separator);
+        } else if (v.ctor === "[]") {
+            return "[]";
+        } else if (v.ctor === "RBNode" || v.ctor === "RBEmpty") {
+            var cons = F3(function(k,v,acc){return NList.Cons(Tuple2(k,v),acc)});
+            var list = A3(Dict.foldr, cons, NList.Nil, v);
+            var name = "Dict";
+            if (list.ctor === "::" && list._0._1.ctor === "_Tuple0") {
+                name = "Set";
+                list = A2(List.map, function(x){return x._0}, list);
+            }
+            return name + ".fromList " + toString(list, separator);
+        } else {
+            var output = "";
+            for (var i in v) {
+                if (i === 'ctor') continue;
+                var str = toString(v[i], separator);
+                var parenless = str[0] === '{' || str[0] === '<' || str.indexOf(' ') < 0;
+                output += ' ' + (parenless ? str : '(' + str + ')');
+            }
+            return v.ctor + output;
+        }
+    }
+    if (type === 'object' && 'recv' in v) return '<signal>';
+    return "<internal structure>";
+};
+
+function addSlashes(str) {
+    return str.replace(/\\/g, '\\\\')
+              .replace(/\n/g, '\\n')
+              .replace(/\t/g, '\\t')
+              .replace(/\r/g, '\\r')
+              .replace(/\v/g, '\\v')
+              .replace(/\0/g, '\\0')
+              .replace(/\'/g, "\\'")
+              .replace(/\"/g, '\\"');
+}
+
+function probablyPublic(v) {
+    var keys = Object.keys(v);
+    var len = keys.length;
+    if (len === 3
+        && 'props' in v
+        && 'element' in v) return false;
+    if (len === 5
+        && 'horizontal' in v
+        && 'vertical' in v
+        && 'x' in v
+        && 'y' in v) return false;
+    if (len === 7
+        && 'theta' in v
+        && 'scale' in v
+        && 'x' in v
+        && 'y' in v
+        && 'alpha' in v
+        && 'form' in v) return false;
+    return true;
 }
