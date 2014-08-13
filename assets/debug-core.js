012f3fbda9e489800a228b9fc30fe04fbf99c535
Michael James <michael@michaelbjames.com>
2014-08-13 15:16:23 +0200

    hotswap button optionally visible by port

--- assets/debug-core.js
+++ assets/debug-core.js
@@ -131,7 +131,8 @@ function initDebugger() {
 
     debuggerHandle = Elm.embed(Elm.DebuggerInterface, debuggerDiv,
         { eventCounter: 0,
-          watches: []
+          watches: [],
+          showHotswap: false
         });
     debuggerHandle.ports.scrubTo.subscribe(scrubber);
     debuggerHandle.ports.pause.subscribe(elmPauser);
