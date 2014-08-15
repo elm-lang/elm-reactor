665d0bce916f2d9a604b43d8c9c14f4489ea72a7
Evan Czaplicki <info@elm-lang.org>
2014-08-15 17:47:40 +0200

    Fix names in command line messages

--- server/Server.hs
+++ server/Server.hs
@@ -33,30 +33,29 @@ data Flags = Flags
 
 flags :: Flags
 flags = Flags
-  { port = 8000 &= help "set the port of the server"
+  { port = 8000 &= help "set the port of the reactor"
   , runtime = Nothing &= typFile
               &= help "Specify a custom location for Elm's runtime system."
-  } &= help "Quickly reload Elm projects in your browser. Just refresh to recompile.\n\
-            \It serves static files and freshly recompiled Elm files."
+  } &= help "Interactive development tool that makes it easy to develop and debug Elm programs.\n\
+            \    Read more about it at <https://github.com/elm-lang/elm-reactor>."
     &= helpArg [explicit, name "help", name "h"]
     &= versionArg [ explicit, name "version", name "v"
                   , summary (Version.showVersion version)
                   ]
-    &= summary ("Elm Server " ++ Version.showVersion version ++
+    &= summary ("Elm Reactor " ++ Version.showVersion version ++
                 ", (c) Evan Czaplicki 2011-2014")
 
 
 config :: Config Snap a
 config = setAccessLog ConfigNoLog (setErrorLog ConfigNoLog defaultConfig)
 
--- | Set up the server.
+-- | Set up the reactor.
 main :: IO ()
 main = do
   cargs <- cmdArgs flags
   (_,Just h,_,_) <- createProcess $ (shell "elm --version") { std_out = CreatePipe }
   elmVer <- hGetContents h
-  putStr $ "Elm Server " ++ Version.showVersion version ++ " serving Elm " ++ elmVer
-  putStrLn "Just refresh a page to recompile it!"
+  putStrLn (startupMessage elmVer)
   httpServe (setPort (port cargs) config) $
       serveRuntime (maybe Elm.runtime id (runtime cargs))
       <|> serveElm
@@ -65,6 +64,11 @@ main = do
       <|> serveDirectoryWith directoryConfig "."
       <|> serveAssets
       <|> error404
+
+startupMessage :: String -> String
+startupMessage elmVer =
+  "Elm Reactor " ++ Version.showVersion version ++ ", backed by version " ++
+  filter (/= '\n') elmVer ++ " of the compiler."
 
 directoryConfig :: MonadSnap m => DirectoryConfig m
 directoryConfig = fancyDirectoryConfig {indexGenerator = elmIndexGenerator}
