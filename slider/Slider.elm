e0f839798c37dd7d8ba443edb8964e348b0d6028
Evan Czaplicki <info@elm-lang.org>
2014-09-21 18:48:32 -0700

    Use commas at beginning of the line

--- slider/Slider.elm
+++ slider/Slider.elm
@@ -14,39 +14,39 @@ import Graphics.Input (Handle)
 {-| The attributes of a slider. This lets you customize a slider to fit however
 you want. You may also modify the default slider style with record updates.
 -}
-type SliderStyle = {
-  horizontal : Bool,
-  disabled : Bool,
-  length : Int,
-  min : Float,
-  max : Float,
-  step : Float,
-  value : Float
- }
+type SliderStyle =
+    { horizontal : Bool
+    , disabled : Bool
+    , length : Int
+    , min : Float
+    , max : Float
+    , step : Float
+    , value : Float
+    }
 
 {-| The default range slider attributes. This is meant to be modified with record
 updates to suite your needs.
 
-      defaultStyle = {
-        horizontal = True,
-        disabled = False,
-        length = 100,
-        min = 0,
-        max = 100,
-        step = 1,
-        value = 0
-       }
+      defaultStyle =
+          { horizontal = True
+          , disabled = False
+          , length = 100
+          , min = 0
+          , max = 100
+          , step = 1
+          , value = 0
+          }
 -}
 defaultSlider : SliderStyle
-defaultSlider = {
-  horizontal = True,
-  disabled = False,
-  length = 100,
-  min = 0,
-  max = 100,
-  step = 1,
-  value = 0
- }
+defaultSlider =
+    { horizontal = True
+    , disabled = False
+    , length = 100
+    , min = 0
+    , max = 100
+    , step = 1
+    , value = 0
+    }
 
  {-| Create a range slider. The following slider lets you choose your height in
 meters with centimeter accuracy (0.01).
