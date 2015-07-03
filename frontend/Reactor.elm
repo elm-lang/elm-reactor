fe2b039a651de02739ba223290e9d4b0ccbb7fa2
Pete Vilter <pete.vilter@gmail.com>
2015-07-02 18:45:25 -0700

    update state type; add action type; build out ports

--- frontend/Reactor.elm
+++ frontend/Reactor.elm
@@ -1,18 +1,41 @@
 module Reactor where
 
+import Dict as D
+import Array as A
+import Time
+import Task as T
+import Signal
+
 type alias DebugState =
     { runningState : RunningState
     -- , totalTimeLost : ?
-    , events : Array Event
-    , snapshots = Array Snapshot
+    , events : A.Array Event
+    , snapshots : A.Array Snapshot
+    , timeStarted : Time
     -- async callbacks?
     -- traces : ?
     , permitSwaps : Bool
+    , sidebarVisible : Bool
     }
 
+type Action
+    -- UI actions
+    = Restart
+    | Pause Bool
+    | ScrubPosition FrameIndex
+    | SidebarVisible Bool
+    | PermitSwap Bool
+    | RestartButtonAction Button.Action
+    | PlayPauseButtonAction Button.Action
+    -- events from RTS
+    | NewEvent Event
+    | NewSnapshot Snapshot
+    | UpdateWatch String String
+    | NoOp
+
 type RunningState
-    = Running
-    | Paused Time FrameIndex -- ?
+    = Running Time
+    | Paused Time FrameIndex
 
 type alias FrameIndex = Int
 
@@ -45,23 +68,56 @@ type alias SignalValue =
     , value : ElmValue
     }
 
+emptySnapshot : Snapshot
+emptySnapshot =
+  { signalGraph = D.emtpy
+  , watches = D.empty
+  }
+
 -- WATCHES
 
 type alias WatchSnapshot = Dict WatchId ElmValue
 
 type alias WatchId = String
 
+-- Update
+
+update : Action -> DebugState -> DebugState
+update action state =
+  ...
+
 -- incoming
 
 port events : Signal Event
 
+port snapshots : Signal SGSnapshot
+
+port watchUpdates : Signal (String, String)
+
 -- outgoing
 
-port captureSnapshot : Signal (Task e Snapshot)
-port captureSnapshot = ...
+captureSnapshotMailbox : Signal.Mailbox (T.Task String ())
+captureSnapshotMailbox =
+  Signal.mailbox T.succeed
+
+port captureSnapshot : Signal (T.Task String ())
+port captureSnapshot =
+  captureSnapshotMailbox.signal
+
+
+setToSnapshotMailbox : Signal.Mailbox Snapshot
+setToSnapshotMailbox =
+  Signal.mailbox emptySnapshot
 
 port setToSnapshot : Signal Snapshot
-port setToSnapshot = ...
+port setToSnapshot =
+  setToSnapshotMailbox.signal
 
-port processEvents : Signal (Array Event)
-port processEvents = ...
+
+processEventsMailbox : Signal.Mailbox (A.Array Event)
+processEventsMailbox =
+  Signal.mailbox A.empty
+
+port processEvents : Signal (A.Array Event)
+port processEvents =
+  processEventsMailbox.signal
